41ec4b95408b1f9214aa691f8b3c3983e20c2cb9
NoblePrinceii <farhan.begg@students.makeschool.com>
2020-10-29 15:54:24 -0700

    string prototype working

--- script.js
+++ script.js
@@ -1,45 +1,38 @@
 // capitalize first letter only
 String.prototype.capitalize = function () {
-    return this.charAt(0).toUpperCase() + this.slice(1)
-}
-let str = 'foo'
-console.log(str.capitalize())
-
-
+    return this.charAt(0).toUpperCase() + this.slice(1);
+};
+const str = 'foo';
+console.log(str.capitalize());
 
 // make all character upper case
-String.prototype.allCaps = function (str) {
-    let splitStr = str.toUpperCase().split(' ');
+String.prototype.allCaps = function () {
+    const splitStr = this.toUpperCase().split(' ');
     return splitStr.join(' ');
-}
-console.log(str.allCaps('test another testing something'))
+};
 
-
+console.log('test another testing something'.allCaps());
 
 // makes first character of each word capital
-String.prototype.capitalizeWords = function (str) {
-    let splitStr = str.toLowerCase().split(' ');
-    for (var i = 0; i < splitStr.length; i++) {
+String.prototype.capitalizeWords = function () {
+    const splitStr = this.toLowerCase().split(' ');
+    for (let i = 0; i < splitStr.length; i++) {
         // Assign it back to the array
         splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
     }
     // Directly return the joined string
     return splitStr.join(' ');
-}
-console.log(str.capitalizeWords('test another testing something'))
-
-
+};
+console.log('test another testing something'.capitalizeWords());
 
 // remove extra white space
-function removeExtraSpaces() {
-    let orig = '   foo  1 ';
-    orig = orig.trim().split(' ')
+// String.prototype.removeExtraSpaces = function () {
+//     let orig = '   foo  1 ';
+//     orig = orig.trim().split(' ')
 
-    return orig.join('')
-}
-console.log(removeExtraSpaces())
-
-
+//     return orig.join('')
+// }
+// console.log(removeExtraSpaces())
 
 // // capitalize every other letter
 // function capitalizeWords(input) {
@@ -50,15 +43,65 @@ console.log(removeExtraSpaces())
 // }
 // console.log(capitalizeWords('test'));
 
-
-
 // convert string to kebab case
 String.prototype.kebabCase = function (str) {
     return str
         .match(/[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+/g)
-        .map(x => x.toLowerCase())
+        .map((x) => x.toLowerCase())
         .join('-');
+};
+console.log(str.kebabCase('test another testing something'));
 
-}
-console.log(str.kebabCase('test another testing something'))
+// convert string to camel case
+String.prototype.snakeCase = function (str) {
+    return str
+        .match(/[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+/g)
+        .map((x) => x.toLowerCase())
+        .join('_');
+};
+console.log(str.snakeCase('test another testing something'));
 
+// convert string to camel case
+String.prototype.snakeCase = function (str) {
+    return str
+        .match(/[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+/g)
+        .map((x) => x.toLowerCase())
+        .join('_');
+};
+console.log(str.snakeCase('test another testing something'));
+
+// convert to camel case
+String.prototype.camelCase = function (str) {
+    return str
+        .replace(/\s(.)/g, ($1) => $1.toUpperCase())
+        .replace(/\s/g, '')
+        .replace(/^(.)/, ($1) => $1.toLowerCase());
+};
+
+console.log(str.camelCase('test another testing something'));
+
+String.prototype.shiftl = function (str) {
+    str = str.slice(1);
+
+    return str;
+};
+
+console.log(str.shiftl('test another testing something'));
+
+//
+String.prototype.shiftString = function (str = '', step = 0) {
+    const { length } = str;
+    const index = step % length;
+    const shift = index < 0 ? length + index : index;
+    if (!str || length === 1 || !shift) {
+        return str;
+    }
+    const reverseString = (str) => str.split('').reverse().join('');
+    const newStr = reverseString(str);
+    const s1 = newStr.slice(0, shift);
+    const s2 = newStr.slice(shift);
+    return reverseString(s1) + reverseString(s2);
+};
+// Right Shift
+// StackDev
+console.log(str.shiftString('StackDev', 3)); // evStackD
